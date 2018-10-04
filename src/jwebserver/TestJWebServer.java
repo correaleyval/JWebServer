fd9982081bd6480720d637965aa8170c1b4aa0d3
correaleyval <correaleyval@gmail.com>
2018-10-04 15:43:02 -0400

    Making Web Server

---
+++ src/jwebserver/TestJWebServer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jwebserver;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author luis
+ */
+public class TestJWebServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        JWebServer server = new JWebServer();
+        
+        int port = 8080;
+        
+        server.listen(port);
+    }
+    
+}
